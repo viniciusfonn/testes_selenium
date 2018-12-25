52f31c841b7864aaa5c0c24885e2a2d4d86f6481
vinicius <viniciusfon96@gmail.com>
2018-12-25 18:39:18 -0200

    adição dos testes de text area e text field

--- src/main/java/cursoSelenium/TesteCampoTreinamento.java
+++ src/main/java/cursoSelenium/TesteCampoTreinamento.java
@@ -2,17 +2,35 @@ package cursoSelenium;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCampoTreinamento {
 
 	@Test
-	public void teste() {
+	public void tsteTextField() {
 		 WebDriver driver = new ChromeDriver(); 
 		 driver.manage().window().maximize();
 		 driver.get("file:///" +  System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		
-		// driver.quit();
+		 driver.findElement(By.id("elementosForm:nome")).sendKeys("Texto escrito");
+		 Assert.assertEquals("Texto escrito", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		 
+		driver.quit();
+	}
+	
+	@Test
+	public void testeTextArea() {
+		 WebDriver driver = new ChromeDriver(); 
+		 driver.manage().window().maximize();
+		 driver.get("file:///" +  System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		 driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("teste");
+		Assert.assertEquals("teste",  driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		 
+		 
+		 driver.quit();
 	}
 }
+
